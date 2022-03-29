37df5fd2c695ea31fb150cc0772165401df1e42c
microdotmatrix <liquid.knives.2@gmail.com>
2022-03-29 09:37:39 -0500

    fixed migration issues!

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,75 +1,9 @@
 require('./bootstrap');
 
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-
-// Import GSAP
-import { gsap } from 'gsap';
-
-// Import Luge (Transition, Loading and Parallax FX)
-// import { luge } from '@waaark/luge';
-
-import { jarallax } from "jarallax";
-
-jarallax(document.querySelectorAll(".jarallax"));
-
-// import Swiper JS
-import Swiper from 'swiper/bundle';
-// import Swiper and modules styles
-// import 'swiper/css/bundle';
-import { flip } from 'lodash';
-
-const swiper = new Swiper(".sw", {
-	containerModifierClass: 'sw-',
-	slideActiveClass: 'sw-slide-active',
-	slideBlankClass: 'sw-slide-invisible-blank',
-	slideClass: 'sw-slide',
-	slideDuplicateClass: 'sw-slide-duplicate',
-	slideDuplicateNextClass: 'sw-slide-duplicate-next',
-	slideDuplicatePrevClass: 'sw-slide-duplicate-prev',
-	slideDuplicateActiveClass: 'sw-slide-duplicate-active',
-	slideNextClass: 'sw-slide-next',
-	slidePrevClass: 'sw-slide-prev',
-	slideVisibleClass: 'sw-slide-visible',
-	speed: 1200,
-	parallax: true,
-	loop: true,
-	autoplay: {
-		delay: 6000,
-	},
-	pagination: {
-		el: ".sw-pagination",
-		clickable: true
-	},
-	navigation: {
-		nextEl: ".sw-button-next",
-		prevEl: ".sw-button-prev"
-	},
-	effect: 'creative',
-	creativeEffect: {
-		prev: {
-			origin: "left center",
-			translate: ["-0.5%", "1%", -200],
-			scale: 0.98,
-		},
-		next: {
-			origin: "left center",
-			translate: ["10%", "1%", -50],
-			scale: 1.1,
-			opacity: 0.5,
-		 },
-
-	},
+var rellax = new Rellax('.rellax', {
+  speed: 4,
+  center: true,
+  round: true,
+  vertical: true,
+  horizontal: false
 });
-
-
-// GSAP Animations
-var tween = gsap.from(".gs-01", {
-	duration: 2,
-	delay: 1.2,
-	x: 20,
-	scale: 1.08,
-	transformOrigin: "right top",
-	opacity: 0,
-	ease: "sine.out"
- });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,25 @@
 
 @section('content')
 
-	@include('components.slider')
-
-	<div class="hero">
-
+	<div class="hero" data-flex="row">
+		<div class="hero__box" data-column="6" data-gutter="x/y">
+			<h2>Save me, Jeebus.</h2>
+			<h5 class="sub">Kids, you tried your best and you failed miserably. The lesson is, never try.</h5>
+			<p> How could you?! Haven't you learned anything from that guy who gives those sermons at church? Captain Whatshisname? We live in a society of laws! Why do you think I took you to all those Police Academy movies? For fun? Well, I didn't hear anybody laughing, did you? Except at that guy who made sound effects. Makes sound effects and laughs. Where was I? Oh yeah! Stay out of my booze.</p>
+		</div>
+		<div class="hero__box" data-column="6">
+			<figure class="hero__image">
+				<img src="img/jacko.jpg" class="rellax" alt="Jack O'Lantern">
+			</figure>
+		</div>
 	</div>
 
 	<div class="container">
 
 		<h2>FTW Technologies</h2>
-		<p>This is my body content.</p>
-		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil unde nostrum reiciendis accusamus eos, quas voluptate obcaecati similique nobis omnis illum facere odit cumque temporibus earum quibusdam dolorem velit sint!</p>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rerum velit adipisci mollitia dolorem alias voluptas possimus, tempora modi repellat molestias nam. Delectus ducimus eveniet fugiat eum? Harum, nihil vitae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod repudiandae corrupti porro maxime. Similique sit ducimus aliquam labore! Porro laborum inventore omnis enim odio? Porro dolores quod quibusdam magnam qui.</p>
+		<h4 class="sub">Fame was like a drug. But what was even more like a drug were the drugs.</h5>
+		<p>What good is money if it can't inspire terror in your fellow man? Whoa, slow down there, maestro. There's a *New* Mexico? I like my beer cold, my TV loud and my homosexuals flaming. Marge, just about everything's a sin. Y'ever sat down and read this thing? Technically we're not supposed to go to the bathroom.</p>
+		<h4>Attempted murder? Now honestly, what is that? Do they give a Nobel Prize for attempted chemistry?</h4>
 
 	</div>
 
